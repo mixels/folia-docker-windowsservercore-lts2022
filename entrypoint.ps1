7ac21eb020113cb4a1dde59647de9c8bf53bed2e
Richard MacAleese <gitsource@fiveforone.net>
2025-07-07 04:19:17 +0000

    Update and rename getFolia.ps1 to entrypoint.ps1
    Corrected wrong file name and some mistaken path references

---
+++ entrypoint.ps1
@@ -0,0 +1,17 @@
+Write-Host "Created by Endkind Ender (www.endkind.net) and modified by Mixels"
+
+if (-not (Test-Path -Path "C:\serverfiles\server.jar")) {
+    & "C:\serverfiles\getFolia.ps1"
+    
+    if ($LASTEXITCODE -ne 0) {
+        exit 1
+    }
+}
+
+while ($true) {
+    java -Xms$env:MIN_RAM -Xmx$env:MAX_RAM $env:JAVA_FLAGS -Dcom.mojang.eula.agree=$env:MINECRAFT_EULA -jar C:\serverfiles\server.jar $env:FOLIA_FLAGS --nogui
+    
+    if ($LASTEXITCODE -ne 0) {
+        exit 1
+    }
+}
